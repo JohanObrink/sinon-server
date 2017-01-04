6d03b936b4e2aca9aa1c124449b8a0a00899a7e3
Johan Öbrink <johan.obrink@gmail.com>
2017-01-04 14:49:53 +0100

    Added server.endpoint and switched to chai-http

--- test/HttpServer.js
+++ test/HttpServer.js
@@ -1,9 +1,9 @@
-const supertest = require('supertest-as-promised')(Promise)
 const chai = require('chai')
 const expect = chai.expect
 const {HttpServer} = require(`${process.cwd()}/lib/`)
 const {spy, stub, match} = require('sinon')
 
+chai.use(require('chai-http'))
 chai.use(require('sinon-chai'))
 
 function chain (funcs) {
@@ -14,134 +14,176 @@ before(function () {
 })
 
 describe('HttpServer', () => {
-  let server, request
-  beforeEach(() => {
-    server = new HttpServer()
-    request = supertest(server.server)
-    stub(console, 'warn')
-  })
-  afterEach(() => {
-    console.warn.restore()
-  })
-  it('calls through', () => {
-    const all = Object.assign(spy(), {__name: 'all'})
-    const getIndex = Object.assign(spy(), {__name: 'getIndex'})
-    const secondCall = Object.assign(spy(), {__name: 'secondCall'})
-    const secondGetIndex = Object.assign(spy(), {__name: 'secondGetIndex'})
-
-    server.calls(all)
-    server.withArgs('GET', '/').calls(getIndex).resolves({status: 204})
-    server.onCall(1).calls(secondCall)
-    server.withArgs('GET', '/').onCall(1).calls(secondGetIndex)
-
-    return chain([
-      () => {
-        return request.get('/').expect(204)
-      },
-      () => {
-        return request.post('/').expect(404)
-      },
-      () => {
-        return request.get('/').expect(204)
-      },
-      () => {
-        return request.get('/').expect(204)
+  describe('constructor', () => {
+    let server
+    afterEach(() => {
+      if (server.running) {
+        return server.stop()
       }
-    ])
-    .then(() => {
-      expect(all.callCount, 'all').to.equal(4)
-      expect(all, 'all').calledWith('GET', '/').and.calledWith('POST', '/')
-
-      expect(getIndex.callCount, 'getIndex').to.equal(3)
-      expect(getIndex, 'getIndex').calledWith('GET', '/').and.not.calledWith('POST', '/')
-
-      expect(secondCall, 'secondCall').calledOnce.calledWith('POST', '/')
-
-      expect(secondGetIndex, 'secondGetIndex').calledOnce.calledWith('GET', '/')
+    })
+    it('sets the correct defaults', () => {
+      server = new HttpServer()
+      expect(server.endpoint).to.equal('http://localhost:1337')
+    })
+    it('uses options', () => {
+      server = new HttpServer({
+        protocol: 'https',
+        host: 'foobar',
+        port: 4000
+      })
+      expect(server.endpoint).to.equal('https://foobar:4000')
+    })
+    it('accepts overrides', () => {
+      server = new HttpServer({protocol: 'https'})
+      return server
+        .start({
+          host: 'foobar',
+          port: 4000
+        })
+        .then(server => {
+          expect(server.endpoint).to.equal('https://foobar:4000')
+        })
     })
   })
-  it('warns of unspecified implementations', () => {
-    return request
-      .get('/')
-      .expect(404)
-      .then(() => {
-        expect(console.warn)
-          .calledOnce
-      })
-  })
-  it('calls the stub correctly', () => {
-    const method = 'POST'
-    const url = '/foo'
-    const headers = {accept: 'application/json'}
-    const body = {foo: 'bar'}
+  describe('calls', () => {
+    let server, request
+    beforeEach(() => {
+      server = new HttpServer({port: 1337})
+      request = chai.request(server.endpoint)
+      stub(console, 'warn')
 
-    return request
-      .post('/foo')
-      .set('Accept', 'application/json')
-      .send({foo: 'bar'})
-      .expect(404)
+      return server.start()
+    })
+    afterEach(() => {
+      console.warn.restore()
+      return server.stop()
+    })
+    it('calls through', () => {
+      const all = Object.assign(spy(), {__name: 'all'})
+      const getIndex = Object.assign(spy(), {__name: 'getIndex'})
+      const secondCall = Object.assign(spy(), {__name: 'secondCall'})
+      const secondGetIndex = Object.assign(spy(), {__name: 'secondGetIndex'})
+
+      server.calls(all)
+      server.withArgs('GET', '/').calls(getIndex).resolves({status: 204})
+      server.onCall(1).calls(secondCall)
+      server.withArgs('GET', '/').onCall(1).calls(secondGetIndex)
+
+      return chain([
+        () => {
+          return request.get('/')
+            .then(res => expect(res).to.have.status(204))
+        },
+        () => {
+          return request.post('/')
+            .catch(res => expect(res).to.have.status(404))
+        },
+        () => {
+          return request.get('/')
+            .then(res => expect(res).to.have.status(204))
+        },
+        () => {
+          return request.get('/')
+            .then(res => expect(res).to.have.status(204))
+        }
+      ])
       .then(() => {
-        expect(server.stub)
-          .calledOnce
-          .calledWith(method, url, match(headers), JSON.stringify(body))
+        expect(all.callCount, 'all').to.equal(4)
+        expect(all, 'all').calledWith('GET', '/').and.calledWith('POST', '/')
+
+        expect(getIndex.callCount, 'getIndex').to.equal(3)
+        expect(getIndex, 'getIndex').calledWith('GET', '/').and.not.calledWith('POST', '/')
+
+        expect(secondCall, 'secondCall').calledOnce.calledWith('POST', '/')
+
+        expect(secondGetIndex, 'secondGetIndex').calledOnce.calledWith('GET', '/')
       })
-  })
-  it('returns the correct default response', () => {
-    return request
-      .get('/')
-      .set('Accept', 'application/json')
-      .end()
-      .then(response => {
-        expect(response.status).to.equal(404)
-        expect(response.text).to.eql('')
-      })
-  })
-  it('returns the correct response', () => {
-    const method = 'POST'
-    const url = '/foo'
-    const headers = {accept: 'application/json'}
-    const body = {foo: 'bar'}
-    const response = {
-      status: 201,
-      headers: {'Content-Type': 'application/json'},
-      body: {
-        herp: 'derp'
+    })
+    it('warns of unspecified implementations', () => {
+      return request
+        .get('/')
+        .catch(res => {
+          expect(res).to.have.status(404)
+          expect(console.warn)
+            .calledOnce
+        })
+    })
+    it('calls the stub correctly', () => {
+      const method = 'POST'
+      const url = '/foo'
+      const headers = {accept: 'application/json'}
+      const body = {foo: 'bar'}
+
+      return request
+        .post('/foo')
+        .set('Accept', 'application/json')
+        .send({foo: 'bar'})
+        .catch(res => {
+          expect(res).to.have.status(404)
+          expect(server.stub)
+            .calledOnce
+            .calledWith(method, url, match(headers), JSON.stringify(body))
+        })
+    })
+    it('returns the correct default response', () => {
+      return request
+        .get('/')
+        .set('Accept', 'application/json')
+        .catch(res => {
+          expect(res).to.have.status(404)
+          expect(res.response.text).to.eql('')
+        })
+    })
+    it('returns the correct response', () => {
+      const method = 'POST'
+      const url = '/foo'
+      const headers = {accept: 'application/json'}
+      const body = {foo: 'bar'}
+      const response = {
+        status: 201,
+        headers: {'Content-Type': 'application/json'},
+        body: {
+          herp: 'derp'
+        }
       }
-    }
-    server
-      .withArgs(method, url, match(headers), JSON.stringify(body))
-      .resolves(response)
-    request
-      .post('/foo')
-      .set('Accept', 'application/json')
-      .send({foo: 'bar'})
-      .expect(201)
-      .then(res => {
-        expect(res.body).to.eql(response.body)
-      })
-  })
-  it('it does not warn for configured calls', () => {
-    const method = 'POST'
-    const url = '/foo'
-    const headers = {accept: 'application/json'}
-    const body = {foo: 'bar'}
-    const response = {
-      status: 201,
-      headers: {'Content-Type': 'application/json'},
-      body: {
-        herp: 'derp'
+      server
+        .withArgs(method, url, match(headers), JSON.stringify(body))
+        .resolves(response)
+      return request
+        .post('/foo')
+        .set('Accept', 'application/json')
+        .send({foo: 'bar'})
+        .then(res => {
+          expect(res).to.have.status(201)
+          expect(res.body).to.eql(response.body)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    })
+    it('it does not warn for configured calls', () => {
+      const method = 'POST'
+      const url = '/foo'
+      const headers = {accept: 'application/json'}
+      const body = {foo: 'bar'}
+      const response = {
+        status: 201,
+        headers: {'Content-Type': 'application/json'},
+        body: {
+          herp: 'derp'
+        }
       }
-    }
-    server
-      .withArgs(method, url, match(headers), JSON.stringify(body))
-      .resolves(response)
-    return request
-      .post('/foo')
-      .set('Accept', 'application/json')
-      .send({foo: 'bar'})
-      .expect(201)
-      .then(() => {
-        expect(console.warn).not.called
-      })
+      server
+        .withArgs(method, url, match(headers), JSON.stringify(body))
+        .resolves(response)
+      return request
+        .post('/foo')
+        .set('Accept', 'application/json')
+        .send({foo: 'bar'})
+        .then(res => {
+          expect(res).to.have.status(201)
+          expect(console.warn).not.called
+        })
+    })
   })
 })
